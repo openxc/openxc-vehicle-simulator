6cdca74e2143d2abaef4052d59cedfcb40b0385c
David Melcher <dmelche6@ford.com>
2013-07-31 18:55:12 -0400

    Send loops organized for minimum code redundancy.

--- state_manager.py
+++ state_manager.py
@@ -10,9 +10,9 @@ class StateManager(object):
         self.connection = enabler_connection.EnablerConnection()
         self.dynamics_model = dynamics_model.DynamicsModel()
 
-        t = threading.Thread(target=self.send_loop_6Hz, name="Thread-6Hz")
-        t.setDaemon(True)
-        t.start()
+        self.start_send_loop(self.send_loop_4Hz, "Thread-4Hz")
+        self.start_send_loop(self.send_loop_6Hz, "Thread-6Hz")
+        self.start_send_loop(self.send_loop_60Hz, "Thread-60Hz")
 
         print('State Manager initialized')
 
@@ -27,22 +27,36 @@ class StateManager(object):
     def accelerator_pedal_position(self, value):
         self.dynamics_model.accelerator = value
 
-    def send_loop_6Hz(self):
-        sleep_duration = 1.0 / (6 * 3)  # 1 second / 6Hz, / # of data points.
+    def start_send_loop(self, function, thread_name):
+        t = threading.Thread(target=self.send_loop, args=[function], name=thread_name)
+        t.setDaemon(True)
+        t.start()
+
+    def send_loop(self, function):
         while True:
             if not self.stopped:
-                self.connection.send_measurement("accelerator_pedal_position",
-                        self.dynamics_model.accelerator)
-                time.sleep(sleep_duration)
-                self.connection.send_measurement("steering_wheel_angle",
-                        self.steering_wheel_angle)
-                time.sleep(sleep_duration)
-                self.connection.send_measurement("vehicle_speed",
-                        self.dynamics_model.vehicle_speed)
-                time.sleep(sleep_duration)
+                function()
             else:
-                time.sleep(1.0 / 6)
+                time.sleep(0.5)
 
+    def send_loop_4Hz(self):
+        SLEEP_4HZ = 1.0 / (4 * 1)  # 1 second / 4Hz, / # of data points.
+        self.connection.send_measurement("vehicle_speed",
+                        self.dynamics_model.vehicle_speed)
+        time.sleep(SLEEP_4HZ)
+
+    def send_loop_6Hz(self):
+        SLEEP_6HZ = 1.0 / (6 * 1)  # 1 second / 6Hz, / # of data points.
+        self.connection.send_measurement("steering_wheel_angle",
+                        self.steering_wheel_angle)
+        time.sleep(SLEEP_6HZ)
+
+    def send_loop_60Hz(self):
+        SLEEP_60HZ = 1.0 / (60 * 1)  # 1 second / 6Hz, / # of data points.
+        self.connection.send_measurement("accelerator_pedal_position",
+                        self.dynamics_model.accelerator)
+        time.sleep(SLEEP_60HZ)
+        
     def pause(self):
         self.stopped = True
 
